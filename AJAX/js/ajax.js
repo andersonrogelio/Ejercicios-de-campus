c5a0f06cfe0474c5201df7826b1acd603ba7efea
Anderson <andersonrogelio0@gmail.com>
2022-07-11 11:15:14 -0300

    Termine el ejemplo con el api de fetch y funciones asincronas

--- AJAX/js/ajax.js
+++ AJAX/js/ajax.js
@@ -13,11 +13,11 @@
             return;
         }
         //mostramos el objeto en consola
-        console.log(xhr);
+        // console.log(xhr);
         //verificamos que el codigo de estado este entre 200 y 299 esto indica que la comunicacion 
         //es correcta y no sucedio ningun error 
         if (xhr.status>= 200 && xhr.status < 300) {
-            console.log("Exito");
+            // console.log("Exito");
             //convertimos el json que traemos del api en objeto y lo guardo en json
             let json = JSON.parse(xhr.responseText);
             // console.log(json);
@@ -36,8 +36,7 @@
             let message = xhr.statusText||"Ocurrio un error";
             $xhr.innerHTML = `Error ${xhr.status}: ${message}`
         }
-        console.log("Este mensaje se mostrara sin importar nada");
-
+        // console.log("Este mensaje se mostrara sin importar nada");
     });
 
     xhr.open("GET","https://jsonplaceholder.typicode.com/users");
@@ -49,15 +48,15 @@
 (() => {
     $fetch = document.getElementById("fetch");
     $fragment = document.createDocumentFragment();
-
+    
     fetch('https://jsonplaceholder.typicode.com/users')
     // .then(res =>{
     // console.log(res);
     // return res.ok ? res.json(): Promise.reject(res);
-// })
-.then((res) => res.ok ? res.json(): Promise.reject(res))
+    // })
+    .then((res) => res.ok ? res.json(): Promise.reject(res))
 .then(json => {
-    console.log(json);
+    // console.log(json);
                 //recorro el json con un forEach 
                 json.forEach(element => {
                     //creo un elemento li 
@@ -71,10 +70,45 @@
                 $fetch.appendChild($fragment);
 })
 .catch(err =>{
-    console.log(err);
+    // console.log(err);
     let message = err.statusText||"Ocurrio un error"
     $fetch.innerHTML = `Error ${err.status}: ${message}`
 }).finally(() => {
-    console.log("Esto se ejecuta independientemente del resultado de la promesa fetch");
+    // console.log("Esto se ejecuta independientemente del resultado de la promesa fetch");
 });
+})();
+
+(() => {
+    $fetchAsync = document.getElementById("fetch-async");
+    $fragment = document.createDocumentFragment();
+    async function getData(){
+        try {
+            let res = await fetch('https://jsonplaceholder.typicode.com/users')
+            json = await res.json();
+            // console.log(res,json);
+            // if (res.of) {
+            //     //el trrow es como un return que envia el flujo de la programacion al catch
+            //     throw new Error("Ocurrio un error al solicitar os datos")
+            // }
+            if(!res.ok) throw {status:res.status, statusText:res.statusText}
+                            //recorro el json con un forEach 
+                            json.forEach(element => {
+                                //creo un elemento li 
+                                const $li = document.createElement("li")
+                                //luego agregamos la info del objeto guardado en la variable json
+                                $li.innerHTML = `${element.name} -- ${element.email} -- ${element.phone}`
+                                //luego agrego el elemento li dentro del fragmento de html que creamos 
+                                $fragment.appendChild($li);
+                            });
+                            //luego agregarlo a mi lista ordenada 
+                            $fetchAsync.appendChild($fragment);
+        } catch (error) {
+            // console.log("Estoy en el catch")
+            let message = error.statusText || "Ocurrio un error";
+            $fetchAsync.innerHTML = `Error ${error.status}: ${message}`
+        }finally{
+            console.log("Esto se ejecuta sin importar el catch")
+        }
+    }
+    getData();
 })();
