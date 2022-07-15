46ac95d45a18374eaece9faeda05bcda843be349
Anderson <andersonrogelio0@gmail.com>
2022-07-15 17:42:20 -0300

    Creacion de la funcion getAll que nos permite acceder y mostrar los datos de mi server en pantalla

--- js/index_FETCH.js
+++ js/index_FETCH.js
@@ -1,3 +1,4 @@
+//Variables que nos permitiran manipular el html
 const d = document;
 $table = d.querySelector(".crud_table");
 $form = d.querySelector(".crud_form");
@@ -5,3 +6,28 @@ $title = d.querySelector(".crud_title");
 $template = d.getElementById("crud_template").content;
 $fragmento = d.createDocumentFragment();
 
+//Primero nos enfocaremos en mostrar los datos que tengamos en nuestro jsonServer
+const getAll = async() => {
+    try {
+        let res = await fetch("http://localhost:3000/santos");
+        json = await res.json();
+        if (!res.ok) throw {status:res.status, statusText:res.statusText} 
+        json.forEach(el => {
+            $template.querySelector(".name").textContent = el.nombre;
+            $template.querySelector(".constellation").textContent = el.constelacion;
+            $template.querySelector(".edit").dataset.id = el.id;
+            $template.querySelector(".edit").dataset.name = el.nombre;
+            $template.querySelector(".edit").dataset.constellation = el.constelacion;
+            $template.querySelector(".delete").dataset.id = el.id;
+            let $clone = d.importNode($template,true);
+            $fragmento.appendChild($clone);
+        });
+        $table.querySelector("tbody").appendChild($fragmento);
+    } catch (error) {
+        let message  = error.statusText || "Ocurrio un error";
+        $table.insertAdjacentHTML("afterend",`<p>Error ${error.status0} ${message}</p>`)
+        
+    }
+}
+
+d.addEventListener("DOMContentLoaded", getAll)
