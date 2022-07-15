086b7b6370b766e985ec9c43df50e6247bce09fe
Anderson <andersonrogelio0@gmail.com>
2022-07-15 17:32:48 -0300

    Creacion de las variables que nos permitiran manupular todo nuestro html

---
+++ js/index_FETCH.js
@@ -0,0 +1,7 @@
+const d = document;
+$table = d.querySelector(".crud_table");
+$form = d.querySelector(".crud_form");
+$title = d.querySelector(".crud_title");
+$template = d.getElementById("crud_template").content;
+$fragmento = d.createDocumentFragment();
+
